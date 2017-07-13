df3fdf6d2dce44b843441bdc8cff293a922f6c93
brandon armand <bwelsh8@gmail.com>
2017-07-13 02:38:23 -0700

    added chatroom

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,16 +1,22 @@
 (function() {
-  function HomeCtrl(Room, $uibModal) {
+  function HomeCtrl(Room, Message, $uibModal) {
     this.roomList = Room.all;
+    this.currentRoom = null;
     this.modalRoom = function(){
       $uibModal.open({
         templateUrl: '/templates/modal.html',
-        size: 'sm',
+        size: 'md',
         controller: 'ModalCtrl as modal'
       });
     }
+
+    this.setRoom = function(room){
+      this.currentRoom = room;
+      this.messages = Message.getByRoomId(this.currentRoom.$id);
+    };
   }
 
   angular
     .module('bloc-chat')
-    .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+    .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
 })();
